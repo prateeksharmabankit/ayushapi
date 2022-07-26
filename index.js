5f85f7d9d9745466de62687be148416cd7709487
Prateek Sharma <prateek.sharma@bankit.in>
2022-07-26 15:47:35 +0530

    aaa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const compression = require('compression')
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({path: "enviornment" + '/.env' });
 
 const mongoString ="mongodb+srv://prateek:V9z1ntUKFeosJLK5@cluster0.aiuci.mongodb.net/proacdoc?retryWrites=true&w=majority";
 
